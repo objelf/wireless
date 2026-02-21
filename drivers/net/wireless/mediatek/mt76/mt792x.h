e7b8bcad5982ba23a1d054e5f295683ba2fa5164
Sean Wang <sean.wang@mediatek.com>
2026-02-21 17:41:36 -0600

    wifi: mt76: mt792x: build iface combinations dynamically for optional NAN
    Expose NAN capability only when supported by firmware. Split chanctx
    interface combinations into base and NAN sets and dynamically append NAN
    combinations when available.
    
    Co-developed-by: Stella Liu <yu-ching.liu@mediatek.com>
    Signed-off-by: Stella Liu <yu-ching.liu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt792x.h
+++ drivers/net/wireless/mediatek/mt76/mt792x.h
@@ -23,6 +23,7 @@
 #define MT792x_CFEND_RATE_11B		0x03	/* 11B LP, 11M */
 
 #define MT792x_FW_TAG_FEATURE	4
+#define MT792x_FW_CAP_NAN	BIT(5)
 #define MT792x_FW_CAP_CNM	BIT(7)
 
 #define MT792x_CHIP_CAP_CLC_EVT_EN BIT(0)
@@ -283,6 +284,9 @@ struct mt792x_dev {
 	u32 backup_l2;
 
 	struct ieee80211_chanctx_conf *new_ctx;
+
+	const struct ieee80211_iface_combination *iface_combinations;
+	int n_iface_combinations;
 };
 
 static inline struct mt792x_bss_conf *
