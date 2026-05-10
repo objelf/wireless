2165726cfb2cf26a743f57c6e9d86276b85ee402
Sean Wang <sean.wang@kernel.org>
2026-05-09 23:32:00 -0500

    wifi: mt76: mt792x: fix USB disconnect cleanup ordering
    Block new TX and MCU traffic during USB disconnect, then stop RX/TX
    before running the WFSYS cleanup reset. Mark the device removed only
    after the hardware cleanup path has finished.
    
    Signed-off-by: Sean Wang <sean.wang@kernel.org>

--- drivers/net/wireless/mediatek/mt76/mt792x_usb.c
+++ drivers/net/wireless/mediatek/mt76/mt792x_usb.c
@@ -169,8 +169,10 @@ EXPORT_SYMBOL_GPL(mt792xu_mcu_power_on);
 static void mt792xu_cleanup(struct mt792x_dev *dev)
 {
 	clear_bit(MT76_STATE_INITIALIZED, &dev->mphy.state);
-	mt792xu_wfsys_reset(dev);
 	skb_queue_purge(&dev->mt76.mcu.res_q);
+	mt76u_stop_rx(&dev->mt76);
+	mt76u_stop_tx(&dev->mt76);
+	mt792xu_wfsys_reset(dev);
 	mt76u_queues_deinit(&dev->mt76);
 }
 
@@ -408,13 +410,26 @@ void mt792xu_disconnect(struct usb_interface *usb_intf)
 {
 	struct mt792x_dev *dev = usb_get_intfdata(usb_intf);
 
+	if (!dev)
+		return;
+
+	set_bit(MT76_RESET, &dev->mphy.state);
+	set_bit(MT76_MCU_RESET, &dev->mphy.state);
+	clear_bit(MT76_STATE_RUNNING, &dev->mphy.state);
+	wake_up(&dev->mt76.mcu.wait);
+	skb_queue_purge(&dev->mt76.mcu.res_q);
+	mt76_worker_disable(&dev->mt76.tx_worker);
+
 	mt792xu_reset_work_cleanup(dev);
 	cancel_work_sync(&dev->init_work);
-	if (!test_bit(MT76_STATE_INITIALIZED, &dev->mphy.state))
+	if (!test_bit(MT76_STATE_INITIALIZED, &dev->mphy.state)) {
+		set_bit(MT76_REMOVED, &dev->mphy.state);
 		return;
+	}
 
 	mt76_unregister_device(&dev->mt76);
 	mt792xu_cleanup(dev);
+	set_bit(MT76_REMOVED, &dev->mphy.state);
 
 	usb_set_intfdata(usb_intf, NULL);
 	usb_put_dev(interface_to_usbdev(usb_intf));
