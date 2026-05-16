defc1b5b4b9a4a6f941694c24cf96970ccc87837
Sean Wang <sean.wang@mediatek.com>
2026-05-15 19:18:01 -0500

    wifi: mt76: mt7925: add NAN MCU helpers
    Co-developed-by: Stella Liu <yu-ching.liu@mediatek.com>
    Signed-off-by: Stella Liu <yu-ching.liu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt792x.h
+++ drivers/net/wireless/mediatek/mt76/mt792x.h
@@ -116,6 +116,20 @@ struct mt792x_link_sta {
 	struct ieee80211_link_sta *pri_link;
 };
 
+struct mt792x_sta_nan_sched {
+	/* protects NAN peer schedule state */
+	spinlock_t lock;
+	u16 committed_dw;
+	u32 sch_idx;
+	bool idx_assigned;
+	unsigned long ndp_ctx_bitmap;
+	u8 ndp_ctx_id;		/* assigned NDP context ID (for NDI sta) */
+	struct {
+		u8 map_id;
+		struct cfg80211_chan_def chans[CFG80211_NAN_SCHED_NUM_TIME_SLOTS];
+	} maps[CFG80211_NAN_MAX_PEER_MAPS];
+};
+
 struct mt792x_sta {
 	struct mt792x_link_sta deflink; /* must be first */
 	struct mt792x_link_sta __rcu *link[IEEE80211_MLD_MAX_NUM_LINKS];
@@ -124,6 +138,9 @@ struct mt792x_sta {
 
 	u16 valid_links;
 	u8 deflink_id;
+
+	/* NAN peer schedule */
+	struct mt792x_sta_nan_sched nan_sched;
 };
 
 DECLARE_EWMA(rssi, 10, 8);
@@ -140,6 +157,26 @@ struct mt792x_bss_conf {
 	unsigned int link_id;
 };
 
+struct mt792x_nan_conf {
+	u8 master_pref;
+	u8 bands;
+	u8 cluster_id[ETH_ALEN];
+	u32 discovery_beacon_interval;
+	bool enable_dw_notification;
+};
+
+struct mt792x_nan {
+	struct mt792x_nan_conf conf;
+
+	/* Scheduler */
+	spinlock_t state_lock;
+	struct cfg80211_chan_def local_sched[CFG80211_NAN_SCHED_NUM_TIME_SLOTS];
+	u32 seq_id;
+
+	/* Connection index bitmap, up to NAN_MAX_CONN_CFG peers */
+	unsigned long conn_bitmap;
+};
+
 struct mt792x_vif {
 	struct mt792x_bss_conf bss_conf; /* must be first */
 	struct mt792x_bss_conf __rcu *link_conf[IEEE80211_MLD_MAX_NUM_LINKS];
@@ -154,6 +191,8 @@ struct mt792x_vif {
 
 	struct work_struct csa_work;
 	struct timer_list csa_timer;
+
+	struct mt792x_nan nan;
 };
 
 struct mt792x_phy {
@@ -287,6 +326,7 @@ struct mt792x_dev {
 
 	const struct ieee80211_iface_combination *iface_combinations;
 	int n_iface_combinations;
+	struct ieee80211_vif *nan_vif;
 };
 
 static inline struct mt792x_bss_conf *
